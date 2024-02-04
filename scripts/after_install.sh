e1d08397ac7a2d280a1adcf641993d22d262d094
paranigit <parani.gobi@gmail.com>
2024-02-04 14:06:28 -0600

    django

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -17,9 +17,9 @@ install requirements.txt
 pip install -r /home/ubuntu/django-aws-cicd-smartmoney/requirements.txt
 
 # run migrations
-screen -d -m python3 manage.py makemigrations
-screen -d -m python3 manage.py migrate
-screen -d -m python3 manage.py collectstatic
+# screen -d -m python3 manage.py makemigrations
+# screen -d -m python3 manage.py migrate
+# screen -d -m python3 manage.py collectstatic
 
 # run server
 screen -d -m python3 manage.py runserver 0:8000
