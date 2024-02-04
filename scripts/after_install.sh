11a53be663ee85465528cd1c09b113df962afe4b
paranigit <parani.gobi@gmail.com>
2024-02-04 14:21:43 -0600

    update script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,18 +3,18 @@
 # kill any servers that may be running in the background 
 sudo pkill -f runserver
 
-# kill frontend servers if you are deploying frontend
+# kill frontend servers if you are deploying any frontend
 # sudo pkill -f tailwind
 # sudo pkill -f node
 
-cd /home/ubuntu/django-aws-cicd-smartmoney/
+cd /home/ubuntu/django-aws_cicd/
 
 # activate virtual environment
 python3 -m venv env
 source env/bin/activate
 
-install requirements.txt
-pip install -r /home/ubuntu/django-aws-cicd-smartmoney/requirements.txt
+# install requirements.txt
+pip install -r /home/ubuntu/django-aws_cicd/requirements.txt
 
 # run server
 screen -d -m python3 manage.py runserver 0:8000
